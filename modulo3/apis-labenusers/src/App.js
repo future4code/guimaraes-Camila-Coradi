6906769453aee5b5868aeda2bf2887bdffdd45bb
CamilaCoradi <camila_coradi@hotmail.com>
2022-02-24 09:40:36 -0300

    commit inicial

---
+++ modulo3/apis-labenusers/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import axios from 'axios';
+;
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <h3>Labenusers</h3>
+
+      </div>
+    )
+  }
+}
