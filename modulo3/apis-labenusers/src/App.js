250e48c798ef39b5dd044375cdedcde407aac1ea
CamilaCoradi <camila_coradi@hotmail.com>
2022-02-24 10:27:57 -0300

    implementacao criar usuario

--- modulo3/apis-labenusers/src/App.js
+++ modulo3/apis-labenusers/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import axios from 'axios';
+import InputUsers from './components/InputUsers';
 ;
 
 export default class App extends React.Component{
+
+
   render(){
     return(
       <div>
         <h3>Labenusers</h3>
+        <button>Trocar de tela</button>
+        <InputUsers/>
 
       </div>
     )
---
+++ modulo3/apis-labenusers/src/components/InputUsers.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import axios from "axios";
+
+export default class InputUsers extends React.Component {
+  state = {
+    inputNome: "",
+    inputEmail: "",
+  };
+  addNome = (event) => {
+    this.setState({ inputNome: event.target.value });
+  };
+
+  addEmail = (event) => {
+    this.setState({ inputEmail: event.target.value });
+  };
+
+  criaUser = () => {
+    const body = {
+      nome: this.state.inputNome,
+      email: this.state.inputEmail,
+    };
+
+    axios
+      .post(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
+        body,
+        {
+          headers: {
+            Authorization: "camila-leal-guimaraes",
+          },
+        }
+      )
+      .then((res) => {
+        alert("Usuário criado com sucesso!");
+      })
+      .catch((err) => {
+        alert("Erro ao criar o usuário!");
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Criar Usuários</p>
+        <div>
+            <input placeholder="nome" value={this.state.inputNome} onChange={this.addNome}/>
+            <input placeholder="email" value={this.state.inputEmail} onChange={this.addEmail}/>
+            <button onClick={this.criaUser} > Criar </button>
+        </div>
+      </div>
+    );
+  }
+}
