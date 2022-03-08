e28be5965e79ad72f930b3d521ceb97e82e06bc6
CamilaCoradi <camila_coradi@hotmail.com>
2022-03-07 21:34:08 -0300

    Criando components de criar Playlist

---
+++ modulo3/projeto-labefy/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import CriarPlayList from './components/CriarPlayList'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div><CriarPlayList/></div>
+    )
+  }
+}
+
