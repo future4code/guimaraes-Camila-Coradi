b061f64c24c04a33c9d0f2330a092fd73d8ceb9f
CamilaCoradi <camila_coradi@hotmail.com>
2022-03-17 14:25:44 -0300

    resolucao use-Effect, sem infos dos pokemons aparecendo

--- modulo4/use-effect/src/App.js
+++ modulo4/use-effect/src/App.js
@@ -1,7 +1,49 @@
-import React from "react";
+import axios from "axios";
+import React,{useState, useEffect}from "react";
+import PokeCard from "./components/PokeCard/PokeCard"
+import {Div} from"./components/style"
+
+
+
+
 
 function App() {
-  return <div></div>;
+
+  const [ pokeList, setPokeList] = useState ([])
+  const [pokeName, setPokeName] = useState('')
+
+  const changePokeName = (event) => {
+    setPokeName(event.target.value);
+  }
+
+  useEffect(()=>{
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then(response => {
+        setPokeList(response.data.results);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    })
+
+ 
+  return (
+    <Div >
+      <select onChange={changePokeName}>
+          <option value={""}>Nenhum</option>
+          {pokeList.map(pokemon => {
+            return (
+              <option key={pokemon.name} value={pokemon.name}>
+                {pokemon.name}
+              </option>
+            );
+          })}
+        </select>
+        {pokeName && <PokeCard pokemon={pokeName} />}
+      
+    </Div>
+  );
 }
 
 export default App;
---
+++ modulo4/use-effect/src/components/style.js
@@ -0,0 +1,11 @@
+import styled from"styled-components"
+import Pikachu from "../assets/pikachu.jpg"
+
+export const Div=styled.div`
+font-family: sans-serif;
+text-align: center;
+background-image:url(${Pikachu});
+color: white;
+height:87.890625vw;
+
+`
