7fdc6813a7ffde26ecc2ad16cd1bf729f7f7297e
CamilaCoradi <camila_coradi@hotmail.com>
2022-04-05 21:47:48 -0300

    botão de login com autenticação

--- modulo4/projeto-labex/src/pages/LoginPage.js
+++ modulo4/projeto-labex/src/pages/LoginPage.js
@@ -1,16 +1,47 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
   const navigate = useNavigate();
-  const goToAdmin = () => navigate("/admin/trips/list");
+  const goToHomePage = () => navigate(-1);
+  const [email, setEmail] = useState('')
+  const [password, setPassword]= useState('')
+
+  const handleEmail=(e)=>{
+    setEmail(e.target.value)
+  }
+
+  const handlePassword=(e)=>{
+    setPassword(e.target.value)
+  }
+
+  const login = ()=>{
+    console.log(email, password)
+    const body ={
+        email: email,
+        password:password
+    }
+    axios.post('https://us-central1-labenu-apis.cloudfunctions.net/labeX/camila-leal-guimaraes/login', 
+    body)
+    .then((res)=> {
+      localStorage.setItem("token", res.data.token)
+      navigate("/admin/trips/list")
+    })
+    .catch((err) =>{
+      alert('Erro: Usuário não encontrado')
+
+    })
+  }
 
   return (
     <div>
       <h1>LoginPage</h1>
-      <input placeholder="email" />
-      <input placeholder="senha" />
-      <button onClick={goToAdmin}>Login</button>
+      <input placeholder="e-mail" value={email} onChange={handleEmail}/>
+      <input placeholder="senha" value={password} onChange={handlePassword} />
+      <button onClick={login} >Login</button>
+      <button onClick={goToHomePage}>Voltar</button>
+      
     </div>
   );
 }
--- modulo4/projeto-labex/src/pages/TripDetailsPage.js
+++ modulo4/projeto-labex/src/pages/TripDetailsPage.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function TripDetailsPage() {
   const navigate = useNavigate();
   const goToHomePage = () => navigate(-1);
+  useEffect(()=>{
+    axios
+    .get('https://us-central1-labenu-apis.cloudfunctions.net/labeX/camila-leal-guimaraes/trip/:id')
+    .then()
+    .catch()
+  }, [])
 
   return (
     <div>
