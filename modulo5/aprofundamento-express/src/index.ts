7d24fa8a49cca4ff2b0e05e88957960d0a6d65f8
CamilaCoradi <camila_coradi@hotmail.com>
2022-06-03 12:03:14 -0300

    post

--- modulo5/aprofundamento-express/src/index.ts
+++ modulo5/aprofundamento-express/src/index.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from "express";
+import cors from "cors"
 
 import { AddressInfo } from "net";
 import { todo } from "./data";
+import { match } from "assert";
 
 const app = express();
+app.use(cors());
 
 app.get("/ping", (req: Request, res: Response) => {
   res.send("Pong! 🏓");
@@ -23,11 +26,18 @@ app.get("/todos/", (req: Request, res: Response) => {
   }
 });
 
-app.post("/todos/:id", (req:Request, res:Response)=>{
+app.post("/todos/", (req:Request, res:Response)=>{
  try{
+const {title, completed} = req.body
+todo.push({
+  userId: Date.now(),
+  id: Date.now(),
+  title,
+  completed
+}); res.status(200).end(todo)
 
  }catch(error){
-
+  res.status(400).end("Adicione uma nova tarefa");
  }
 })
 
