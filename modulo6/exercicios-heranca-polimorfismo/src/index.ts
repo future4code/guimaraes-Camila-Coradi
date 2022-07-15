87220b5f9709a9eba5e6162786654f6b88c01084
CamilaCoradi <camila_coradi@hotmail.com>
2022-07-14 23:08:52 -0300

    Exercício polimorfismo 4 ao 6

--- modulo6/exercicios-heranca-polimorfismo/src/index.ts
+++ modulo6/exercicios-heranca-polimorfismo/src/index.ts
@@ -126,72 +126,160 @@ export abstract class Place {
 
 // const place = new Place("05038040")
 
-//Exercício 2
+//Exercício 2 e 3
 
 //A - O erro que o TS gerou foi o seguinte: Não é possivel criar uma instância de uma classe abstrata ts (2511)
 
 //B - Para criar a instância da classe Place é estendê-la a outras classes para utilizar a propriedade
 
 export class Residence extends Place {
-    constructor(
-      protected residentsQuantity: number,
-      // Refere-se ao número de moradores da casa
-  
-      cep: string
-    ) {
-      super(cep);
-    }
-    public getDwellersQuantity(): number {
-        return this.residentsQuantity
-      }
-  };
+  constructor(
+    protected residentsQuantity: number,
+    // Refere-se ao número de moradores da casa
 
-  export class Commerce extends Place {
-    constructor(
-      protected floorsQuantity: number,
-      // Refere-se à quantidade de andares do lugar
-  
-      cep: string
-    ) {
-      super(cep);
-    }
+    cep: string
+  ) {
+    super(cep);
+  }
+  public getDwellersQuantity(): number {
+    return this.residentsQuantity;
+  }
+}
 
-    public getFloorsQuantity(): number {
-        return this.floorsQuantity;
-      }
-  };
+export class Commerce extends Place {
+  constructor(
+    protected floorsQuantity: number,
+    // Refere-se à quantidade de andares do lugar
 
-  export class Industry extends Place {
-    constructor(
-      protected machinesQuantity: number, 
-      // Refere-se à quantidade de máquinas do local 
-      
-      cep: string
-          ) {
-          super(cep);
-    }
-
-    public getMachinesQuantity(): number {
-        return this.machinesQuantity;
-      }
+    cep: string
+  ) {
+    super(cep);
   }
 
-  const residence = new Residence(4, "05038040")
-  console.log(residence)
-  
-  const commerce = new Commerce(8, "05061150")
-  console.log(commerce)
+  public getFloorsQuantity(): number {
+    return this.floorsQuantity;
+  }
+}
 
-  const industry = new Industry(6, "00578030")
-  console.log(industry)
+export class Industry extends Place {
+  constructor(
+    protected machinesQuantity: number,
+    // Refere-se à quantidade de máquinas do local
 
-  residence.getDwellersQuantity()
-  console.log(residence)
+    cep: string
+  ) {
+    super(cep);
+  }
 
-  commerce.getFloorsQuantity()
-  console.log(residence)
+  public getMachinesQuantity(): number {
+    return this.machinesQuantity;
+  }
+}
 
-  industry.getMachinesQuantity()
-  console.log(industry)
+const residence = new Residence(4, "05038040");
+console.log(residence);
 
-  
+const commerce = new Commerce(8, "05061150");
+console.log(commerce);
+
+const industry = new Industry(6, "00578030");
+console.log(industry);
+
+residence.getDwellersQuantity();
+console.log(residence);
+
+commerce.getFloorsQuantity();
+console.log(residence);
+
+industry.getMachinesQuantity();
+console.log(industry);
+
+//Exercício 4
+
+class ResidentialClient extends Residence implements Client {
+  constructor(
+    public name: string,
+    public registrationNumber: number,
+    public consumedEnergy: number,
+    private cpf: string,
+    residentsQuantity: number,
+    cep: string
+  ) {
+    super(residentsQuantity, cep);
+  }
+
+  public getCpf(): string {
+    return this.cpf;
+  }
+
+  public calculateBill(): number {
+    return this.consumedEnergy * 0.75;
+  }
+}
+
+const client1 = new ResidentialClient(
+  "teste4",
+  5,
+  150,
+  "65560078905",
+  4,
+  "18797547"
+);
+console.log(client1);
+
+//A - Possui 2 metodos um getter para pegar o CPF que é uma propriade "private" e o outro metodo para o calculo do consumo
+
+//Exercício 5
+
+class CommercialClient extends Commerce implements Client {
+  constructor(
+    public name: string,
+    public registrationNumber: number,
+    public consumedEnergy: number,
+    private cnpj: string,
+    floorsQuantity: number,
+    cep: string
+  ) {
+    super(floorsQuantity, cep);
+  }
+
+  public calculateBill(): number {
+    return this.consumedEnergy * 0.53;
+  }
+
+  public getCnpj(): string {
+    return this.cnpj;
+  }
+}
+
+//A - Ambas classes possuem o metodo getter que pega uma propriedade "private" (CPF e CNPJ), também possuem semelhanças referente ao metodo que faz o calculo de consumo
+
+//B - Uma das diferenças é a classe pai
+
+//Exercício 6
+
+class IndustrialClient  extends Industry implements Client {
+  constructor(
+    public name: string,
+    public registrationNumber: number,
+    public consumedEnergy: number,
+    private insdustryNumber: string, // tanto faz ser string ou number
+    machinesQuantity: number,
+    cep: string
+  ) {
+    super(machinesQuantity, cep);
+  }
+
+  public getIndustryNumber(): string {
+    return this.insdustryNumber;
+  }
+
+  public calculateBill(): number {
+    return this.consumedEnergy * 0.45 + this.machinesQuantity * 100;
+  }
+}
+//A - Ela deve ser filha da classe Industry pois possui as informações referentes a essa classe
+
+//B - Implementa a interface de client por que é um cliente da companhia de energia
+
+//C - Para pegar as propriedades private
