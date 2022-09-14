88e8dfdb90879032ff1993514001ddd55bb4e0fe
CamilaCoradi <camila_coradi@hotmail.com>
2022-09-13 21:32:48 -0300

    exercicíos

---
+++ modulo7/resolvendo-problemas/src/exercise2/index.ts
@@ -0,0 +1,33 @@
+export const stringCompression = (input:any) => {
+   const substrings = [];
+   let lastChar = input[0];
+   let charCount = 0;
+ 
+   for (const char of input) {
+     if (char !== lastChar) {
+       substrings.push(lastChar + charCount);
+       lastChar = char;
+       charCount = 0;
+     }
+     charCount++;
+   }
+ 
+   substrings.push(lastChar + charCount);
+   let result = "";
+   for (const key of substrings) {
+     result += key;
+   }
+ 
+   return result.length > input.length ? input : result;
+ };
+
+ console.log(stringCompression("aabbb")),
+ console.log(stringCompression("aabcccccaaa")),
+ console.log(stringCompression("accurate")),
+ console.log(stringCompression("a1c2u1r1a1t1e1")),
+ console.log(stringCompression("escola")),
+ console.log(stringCompression("accuraaaaaaaaaate"))
+
+ 
+
+
